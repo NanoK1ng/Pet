9aaedc575559d1cf1503dff1866717a60f08d892
Vladimir <nanokoroll321@gmail.com>
2023-06-10 12:29:38 +0200

    fixed bug/ final version

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -1,12 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu (fileName = "Money", order = 1)]
 public class Money : ScriptableObject
 {
-    public int Coins;
+    public int Coins = 0;
 
+    private const string FILENAME = "Money.dat";
+
+    public void SaveToFile()
+    {
+        var filePath = Path.Combine(Application.persistentDataPath, FILENAME);
+
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath);
+        }
+
+        var json = JsonUtility.ToJson(this);
+        File.WriteAllText(filePath, json);
+    }
+
+
+    public void LoadDataFromFile()
+    {
+        var filePath = Path.Combine(Application.persistentDataPath, FILENAME);
+
+        if (!File.Exists(filePath))
+        {
+            Debug.LogWarning($"File \"{filePath}\" not found!", this);
+            return;
+        }
+
+        var json = File.ReadAllText(filePath);
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
 }
